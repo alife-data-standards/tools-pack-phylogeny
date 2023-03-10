685ec8856dfafa472d8c4b99ccf8ce98207cfe62
Emily Dolson <emilyldolson@gmail.com>
2023-03-09 23:25:53 -0500

    Add step column

--- phylogeny_tools/agg_phylogenies.py
+++ phylogeny_tools/agg_phylogenies.py
@@ -1,15 +1,28 @@
-import matplotlib.pyplot as plt
 import pandas as pd
 import glob
 import sys
-import numpy as np
 import click
+import re
+
 
 @click.command()
-@click.option('--cutoff', default=500, help='Minimum population size to include.')
+@click.option('--cutoff', default=500,
+              help='Minimum population size to include.')
+@click.option('--time_regex', default="[0-9]+",
+              help="""A regular expression that will extract the time step of
+                      a data point from a filename.""")
+@click.option('--output_file_root', default="all_phylogeny",
+              help='String to start output file names with.')
 @click.argument('pattern')
-def aggregate(pattern, cutoff):
+def aggregate_muller_data_to_file(pattern, cutoff,
+                                  time_regex, output_file_root):
+    all_edge_data, all_size_data = \
+        aggregate_muller_data(pattern, cutoff, time_regex)
+    all_edge_data.to_csv(output_file_root + "_edges.csv", index=False)
+    all_size_data.to_csv(output_file_root + "_sizes.csv", index=False)
 
+
+def aggregate_muller_data(pattern, cutoff, time_regex):
     edge_dfs = []
     size_dfs = []
 
@@ -23,6 +36,7 @@ def aggregate(pattern, cutoff):
         size_df = pd.DataFrame()
         size_df["Id"] = df["id"]
         size_df["filename"] = filename
+        size_df["Step"] = re.search(time_regex, filename).group(0)
         size_df["Pop"] = df["num_orgs"]
         size_df["info"] = df["info"]
         edge_dfs.append(df)
@@ -30,12 +44,12 @@ def aggregate(pattern, cutoff):
 
     all_edge_data = pd.concat(edge_dfs)
     agg_functions = {"id": "first",
-                    "ancestor_list" : "first",
-                    "origin_time": "first",
-                    "destruction_time": "min",
-                    "info": "first",
-                    "tot_orgs" : "max"
-                    }
+                     "ancestor_list": "first",
+                     "origin_time": "first",
+                     "destruction_time": "min",
+                     "info": "first",
+                     "tot_orgs": "max"
+                     }
     all_edge_data = all_edge_data.groupby(all_edge_data["id"]).aggregate(agg_functions)
     all_edge_data.reset_index(inplace=True, drop=True)
     all_size_data = pd.concat(size_dfs)
@@ -43,12 +57,12 @@ def aggregate(pattern, cutoff):
     all_edge_data.sort_values(axis=0, by="id", inplace=True)
     all_edge_data["ParentId"] = all_edge_data["ancestor_list"].str.strip("[]")
     all_edge_data.rename(columns={"id": "ChildId"}, inplace=True)
-    # all_size_data.sort_values(axis=0, by=["Id", "Step"], inplace=True)
+    all_size_data.sort_values(axis=0, by=["Id", "Step"], inplace=True)
     all_edge_data.replace("NONE", "0", inplace=True)
     all_edge_data["ParentId"] = pd.to_numeric(all_edge_data["ParentId"])
     all_edge_data["ChildId"] = pd.to_numeric(all_edge_data["ChildId"])
     all_size_data["Id"] = pd.to_numeric(all_size_data["Id"])
-    # all_size_data["Step"] = pd.to_numeric(all_size_data["Step"])
+    all_size_data["Step"] = pd.to_numeric(all_size_data["Step"])
     all_size_data["Pop"] = pd.to_numeric(all_size_data["Pop"])
     all_size_data["info"] = pd.to_numeric(all_size_data["info"])
 
@@ -67,7 +81,6 @@ def aggregate(pattern, cutoff):
         all_edge_data.loc[remove_df["ParentId"], "total_offspring"] -= 1
         all_edge_data.drop(remove_df.index, inplace=True)
 
-        #all_edge_data = all_edge_data[(all_edge_data["total_offspring"] > 0) | (all_edge_data["tot_orgs"] > cutoff)]
         remove_df = all_edge_data[(all_edge_data["total_offspring"] <= 0) & (all_edge_data["tot_orgs"] < cutoff) & (all_edge_data["ParentId"] != 0)]
 
     all_edge_data = all_edge_data[(all_edge_data["total_offspring"] > 0) | (all_edge_data["tot_orgs"] > cutoff)]
@@ -75,9 +88,8 @@ def aggregate(pattern, cutoff):
     all_edge_data.reset_index(inplace=True)
     all_size_data = all_size_data[all_size_data["Id"].isin(all_edge_data["ChildId"])]
 
-    all_edge_data.to_csv("all_phylogeny_edges.csv", index=False)
-    all_size_data.to_csv("all_phylogeny_sizes.csv", index=False)
+    return all_edge_data, all_size_data
 
 
 if __name__ == "__main__":
-    aggregate()
+    aggregate_muller_data_to_file()
